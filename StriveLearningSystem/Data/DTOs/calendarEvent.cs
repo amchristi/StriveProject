0f8393ca17942fcb35503df6375aaaa2f8bdcb72
Trevor Spencer <trevor@admonex.com>
2020-02-08 17:04:17 -0700

    Calendar Component created, still needs lots of work.

---
+++ StriveLearningSystem/Data/DTOs/calendarEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.DTOs
+{
+    // based on https://fullcalendar.io/docs/event-object
+    public class CalendarEvent
+    {
+        public string Id { get; set; }
+        public string GroupId { get; set; }
+        public bool AllDay { get; set; } = false;
+        public DateTime? Start { get; set; }
+        public DateTime? End { get; set; }
+        public string Title { get; set; }
+        //public string Url { get; set; }
+        public string[] ClassNames { get; set; }
+    }
+}
---
+++ StriveLearningSystem/StriveLearningSystem/Client/wwwroot/fullcalendar/fullcalendarInterop.js
@@ -0,0 +1,17 @@
+﻿
+window.createNewCalendar = (selector, events) => {
+    var calendarEl = document.getElementById(selector);
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        plugins: ['dayGrid'],
+        events: events
+    });
+
+    calendar.render();
+
+};
+
+window.disposeCalendar = (selector) => {
+    var calendarEl = document.getElementById(selector);
+    calendarEl.destroy();
+};
